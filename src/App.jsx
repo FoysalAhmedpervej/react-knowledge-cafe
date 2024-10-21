88f74f393623c5d4322a180fec3d3504aaa4ba2c
FoysalAhmedpervej <161424548+FoysalAhmedpervej@users.noreply.github.com>
2024-10-21 19:42:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+ 
+  return (
+    <>
+     
+      <h1 className='text-6xl font-bold bg-cyan400'>Knowledge cafe</h1>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
