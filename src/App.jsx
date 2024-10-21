38e2e627080549752638a4b2aa195555403ca5cd
FoysalAhmedpervej <161424548+FoysalAhmedpervej@users.noreply.github.com>
2024-10-21 20:20:56 +0600

    blog section created, blog connected with app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
  
   return (
     <>
-     
-      <h1 className='text-6xl font-bold bg-cyan400'>Knowledge cafe</h1>
+     <Header></Header>
+     <div className='md:flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+     </div>
     </>
   )
 }
